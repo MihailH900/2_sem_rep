92ca0ecdebdf72a0caae03a5adc41b61ec0ec498
MihailH900 <miha.khoroshaev@gmail.com>
2023-04-28 08:44:39 +0300

    first version of lab3_3

--- lab3_3/hash_table.c
+++ lab3_3/hash_table.c
@@ -1,5 +1,6 @@
 #include "hash_table.h"
 #include <stdlib.h>
+#include <string.h>
 
 Hash_table* hash_table_init(size_t capacity)
 {
@@ -32,9 +33,110 @@ Hash_table* hash_table_init(size_t capacity)
 	return h;
 }
 
-char hash_table_add(Hash_table* h, void* key, size_t key_size, void* data, size_t data_size)
+char hash_table_add(Hash_table* h, void* key, size_t key_size, void* data, size_t data_size, size_t version)
 {
+	size_t elem_hash = hash_function(key, key_size) % h->capacity;
+	if (h->key_arr[elem_hash]->busy == 1)
+	{
+		if (h->key_arr[elem_hash]->key_size != key_size || check_equal(h->key_arr[elem_hash]->key_ptr, key, min(h->key_arr[elem_hash]->key_size, key_size) ) )
+		{
+			size_t i = 0;
+			
+			while ( (h->key_arr[elem_hash]->key_size != key_size || check_equal(h->key_arr[elem_hash]->key_ptr, key, min(h->key_arr[elem_hash]->key_size, key_size) ) ) && i < h->capacity )
+			{
+				elem_hash = add_hash(elem_hash, h->capacity) % h->capacity;
+				i++;
+			}
 
+			if (i < h->capacity)
+			{
+				
+			}
+
+			return HASH_TABLE_ADD_SIZE_ERROR;
+		}
+		else
+		{
+			Node* n = malloc_node();
+			if (n == NULL)
+			{
+				return HASH_TABLE_MEMORY_ERROR;
+			}
+
+			n->release = h->key_arr[elem_hash]->variables_list->release+1;
+			
+			n->data->data_ptr = malloc(data_size);
+			if (n->data->data_ptr == NULL)
+			{
+				return HASH_TABLE_MEMORY_ERROR;
+			}
+			memcpy(n->data->data_ptr, data, data_size);
+			
+			n->next = h->key_arr[elem_hash]->variables_list;
+			h->key_arr[elem_hash]->variables_list = n;
+
+			return HASH_TABLE_OK;
+		}
+	}
+	else
+	{
+		h->key_arr[elem_hash]->busy = 1;
+
+		h->key_arr[elem_hash]->key_ptr = malloc(key_size);
+		if (h->key_arr[elem_hash]->key_ptr == NULL)
+		{
+			return HASH_TABLE_MEMORY_ERROR;
+		}
+		memcpy(h->key_arr[elem_hash]->key_ptr, key, key_size);
+		h->key_arr[elem_hash]->key_size = key_size;
+
+		h->key_arr[elem_hash]->variables_list = malloc_node();
+		if (h->key_arr[elem_hash]->variables_list == NULL)
+		{
+			return HASH_TABLE_MEMORY_ERROR;
+		}
+
+		h->key_arr[elem_hash]->variables_list->release = 1;
+
+		h->key_arr[elem_hash]->variables_list->data->data_ptr = malloc(data_size);
+		if (h->key_arr[elem_hash]->variables_list->data->data_ptr == NULL)
+		{
+			return HASH_TABLE_MEMORY_ERROR;
+		}
+		memcpy(h->key_arr[elem_hash]->variables_list->data->data_ptr, data, data_size);
+		
+		h->key_arr[elem_hash]->variables_list->next = NULL;
+
+		return HASH_TABLE_OK;
+	}
+}
+
+char add_node_in_list_of_hash_table(Hast_table* h, size_t elem_hash, void* data_ptr, size_t data_size)
+{
+	Node* n = malloc_node();
+	if (n == NULL)
+	{
+		return HASH_TABLE_MEMORY_ERROR;
+	}
+
+	n->release = h->key_arr[elem_hash]->variables_list->release+1;
+			
+	n->data->data_ptr = malloc(data_size);
+	if (n->data->data_ptr == NULL)
+	{
+		return HASH_TABLE_MEMORY_ERROR;
+	}
+	memcpy(n->data->data_ptr, data, data_size);
+	
+	n->next = h->key_arr[elem_hash]->variables_list;
+	h->key_arr[elem_hash]->variables_list = n;
+
+	return HASH_TABLE_OK;
+}
+
+size_t add_hash(size_t hash, size_t add_val)
+{
+	return (hash + (add_val -1 ) );
 }
 
 size_t hash_function(void* data_ptr, size_t size)
@@ -51,4 +153,51 @@ size_t hash_function(void* data_ptr, size_t size)
 	}
 
 	return res;
+}
+
+Node* malloc_node()
+{
+	Node* n = (Node*) malloc(sizeof(Node));
+	if (n == NULL)
+	{
+		return NULL;
+	}
+
+	n->data = (Item*) malloc(sizeof(Item));
+	if (n->data == NULL)
+	{
+		return NULL;
+	}
+
+	return n;
+}
+
+char check_equal(void* a, void* b, size_t size)
+{
+	char* a_byte = a;
+	char* b_byte = b;
+
+	size_t i = 0;
+	while (i < size)
+	{
+		if ( (*a_byte) != (*b_byte) )
+		{
+			return 1;
+		}
+
+		a_byte++;
+		b_byte++;
+		i++;
+	}
+
+	return 0;
+}
+
+size_t min(size_t a, size_t b)
+{
+	if (a > b)
+	{
+		return b;
+	}
+	return a;
 }
--- lab3_3/hash_table.h
+++ lab3_3/hash_table.h
@@ -7,8 +7,8 @@
 
 typedef struct Item
 {
-	void* data;
-	size_t size;
+	void* data_ptr;
+	size_t data_size;
 } Item;
 
 typedef struct Node
@@ -25,10 +25,10 @@ typedef struct Key
 {
 	char busy;
 	
-	void* key;
+	void* key_ptr;
 	size_t key_size;
 
-	Node* variable_list;
+	Node* variables_list;
 } Key;
 
 typedef struct Hash_table
@@ -39,7 +39,27 @@ typedef struct Hash_table
 	Key** key_arr;
 } Hash_table;
 
+typedef enum Hash_table_elem_states
+{
+	DELETED = -1,
+	FREE,
+	BUSY
+} Hash_table_elem_states;
+
+typedef enum Hash_table_codes
+{
+	HASH_TABLE_OK,
+	HASH_TABLE_MEMORY_ERROR,
+	HASH_TABLE_ADD_SIZE_ERROR,
+	HASH_TABLE_FIND_ERROR
+} Table_codes;
+
 Hash_table* hash_table_init(size_t capacity);
+char hash_table_add(Hash_table* h, void* key, size_t key_size, void* data, size_t data_size, size_t version);
+size_t add_hash(size_t hash, size_t add_val);
 size_t hash_function(void* data_ptr, size_t size);
+Node* malloc_node();
+char check_equal(void* a, void* b, size_t size);
+size_t min(size_t a, size_t b);
 
 #endif
